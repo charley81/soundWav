833b4a71d52b24655265cbb66494f02b72058a7d
ChrisKnowsDev <chrisharley81@gmail.com>
2020-05-28 15:33:00 -0400

    initial commit, server setup, User model, migrated db and seeders file for testing Users

---
+++ seeders/demo_user.js
@@ -0,0 +1,52 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          first_name: 'John',
+          last_name: 'Doe',
+          phone: '555-555-5555',
+          email: 'jdoe@gmail.com',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          first_name: 'Jane',
+          last_name: 'Doe',
+          phone: '222-222-2222',
+          email: 'jdoe2@gmail.com',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          first_name: 'Mike',
+          last_name: 'Jones',
+          phone: '770-990-9087',
+          email: 'mjones@gmail.com',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          first_name: 'Pastor',
+          last_name: 'Troy',
+          phone: '678-009-2354',
+          email: 'ptgb@gmail.com',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          first_name: 'Jim',
+          last_name: 'Smith',
+          phone: '770-887-5436',
+          email: 'jdoe@gmail.com',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    ),
+
+  down: (queryInterface, Sequelize) =>
+    queryInterface.bulkDelete('Users', null, {}),
+};
